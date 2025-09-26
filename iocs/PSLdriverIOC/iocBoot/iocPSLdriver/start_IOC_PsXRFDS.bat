d5eb5e61629eb7ee0e67f2b1c1ae2961c4c2e3c5
MichaS-git <s-micha@gmx.net>
2025-09-26 16:10:02 +0200

    config_site edit

--- iocs/PSLdriverIOC/iocBoot/iocPSLdriver/start_IOC_PsXRFDS.bat
+++
@@ -1,2 +0,0 @@
-cd C:\epics\inst-7.0\areaDetector\ADPSLdriver\iocs\PSLdriverIOC\iocBoot\iocPSLdriver
-C:\epics\inst-7.0\areaDetector\ADPSLdriver\iocs\PSLdriverIOC\bin\windows-x64-static\PSLdriverApp.exe st.cmd
